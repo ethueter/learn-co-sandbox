56f6797bb7198402728068ef9d95609cc4ab0897
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:26:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ first_name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(first_name)
   first_name.each do |list_name|
-  return "Hello, my name is " + list_name + "."
+  puts "Hello, my name is " + list_name + "."
   end
 end
 batch_badge_creator(first_name)
