6ca58b630bac861dfd394c167d6d935eb9c03b2d
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:20:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,6 +9,11 @@ def batch_badge_creator(first_name)
   badge_wording = []
   first_name.each do |list_name|
     badge_wording.push(badge_maker(list_name))
+  end
+  puts batch_badge_creator
+end
+
+
   
 
     
