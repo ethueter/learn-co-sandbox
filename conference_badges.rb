1dd0f7eb5b96aaa72d429ce416291b794e475082
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:38:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ batch_badge_creator(first_name)
 rooms = [1, 2, 3, 4, 5, 6, 7]
 
 def assign_rooms(rooms, index)
-  first_name = first_name[index]
+  name = 
   first_name.each do |room|
     puts "Hello " + first_name[0] + "! You'll be assigned room " + room + "!"
   end
