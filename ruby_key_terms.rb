9e154cba3d8de607669124215a1aec149160763d
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:05:24 +0000

    Automatically backed up by Learn

---
+++ ruby_key_terms.rb
@@ -0,0 +1,12 @@
+# IRB or irb is interactive ruby, this is a way to run/test ruby code without permenently altering anything...ie nothing is saved
+# REPL is  Read Evaluate Print Loop, this is the action that irb takes when a line of code is entered 
+#Data types in ruby... booleans, symbols, numbers, strings, arrays, and hashes 
+# booleans can only be true or false (except if it is nil) note all strings are true
+# symbols will be covered more later 
+# numbers or integers these are actual numbers and are not contained in punctuation and can 
+# ...be used to perform math 
+# strings are any charactors, numbers or words that are enclosed in "" or '' 
+# strings contain literal value and cannot be used for math, also strings are always true
+# arrays are list of things designated by square brackets [] see also arrays.rb 
+# hashes are like arrays but contain 2 pieces of data per item/line and are designated with curly 
+#...brackets with the items in quotes separated by comma
