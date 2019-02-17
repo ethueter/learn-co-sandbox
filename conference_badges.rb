2b877cb224fe36b83654a9d22f3daa961b998584
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:16:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,8 @@ badge_maker("Arel")
 first_name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(first_name)
-  first_name.each do |name|
-    
+  badge_wording = []
+  
+
     
   
