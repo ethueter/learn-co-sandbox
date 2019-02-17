9aaf7087d3c46833c692b43e52fb03a4a06e31aa
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-17 20:15:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,10 @@ def badge_maker(name)
 end
 badge_maker("Arel")
 
-first_name = ["Edsger", "Ada", "Charles", "Alan", ]
+first_name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def batch_badge_creator(first_name)
+  first_name.each do |name|
+    
+    
   
